c8c90f534e8cd84fc8fd23731f2d85a1608143cc
Dheeraj Kumar G <guntidheerajkumar@gmail.com>
2017-01-04 14:51:44 +0530

    iOS Custom Fonts.
    iOS Custom Fonts.

---
+++ SampleCustomFonts/ViewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using UIKit;
+
+namespace SampleCustomFonts
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			var font = UIFont.FromName("ArimaMadurai-Bold", 40f);
+			myLabel.Text = "Dheeraj Kumar G";
+			myLabel.Font = font;
+		}
+	}
+}
